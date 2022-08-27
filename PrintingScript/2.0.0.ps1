4f4f5766133ff0a2b976f26a4dfca002bda2eec1
Robert Meylor <112234783+RMeylor@users.noreply.github.com>
2022-08-27 10:44:11 -0700

    Add files via upload

---
+++ PrintingScript/2.0.0.ps1
@@ -0,0 +1,10 @@
+﻿    $printers = Get-Printer 
+    if ($printers.count -gt 1){
+        Write-Host "Multiple printers were found" -ForegroundColor Green
+        Write-Host "Please select a printer" -ForegroundColor Green
+        for($i = 0; $i -lt $printers.count; $i++){
+            Write-Host "$($i): $($printers[$i].SamAccountName) | $($printers[$i].Name)"
+        }
+        $selection = Read-Host -Prompt "Enter the number of the printer"
+        $selected_printer = $printers[$selection]
+    }
